9db561deb7d03dfed38f8f86ce09b5d556629fb2
Mike <wizywig@gmail.com>
2026-03-02 17:53:10 -0500

    Install yq package in setup-pi.sh
    Added installation steps for yq package in setup script.

--- setup-pi.sh
+++ setup-pi.sh
@@ -40,6 +40,10 @@ log "Installing required packages..."
 apt-get update
 apt-get install -y git python3 python3-pip
 
+log "Installing yq package..."
+wget -qO /usr/local/bin/yq https://github.com/mikefarah/yq/releases/latest/download/yq_linux_arm
+chmod a+x /usr/local/bin/yq
+
 log "Installing ZeroTier..."
 curl -s 'https://raw.githubusercontent.com/zerotier/ZeroTierOne/main/doc/contact%40zerotier.com.gpg' | gpg --import && \
 if z=$(curl -s 'https://install.zerotier.com/' | gpg); then echo "$z" | sudo bash; fi
